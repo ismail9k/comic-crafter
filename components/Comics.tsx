7c74cd57c5060be4f94954c45803d80df816f6ff
ismail9k <dev@ismail9k.com>
2023-08-12 14:08:05 +0300

    feat: ability to mint comics

--- components/Comics.tsx
+++ components/Comics.tsx
@@ -1,15 +1,19 @@
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import comics from "../assets/data/comics.json";
 import font from "../styles/theme/font";
 
 import {
+  Grid,
+  Box,
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
+  Modal,
   Typography,
+  CardActions,
+  Button,
 } from "@mui/material";
+import { useState } from "react";
 
 function excerpt(description) {
   const maxLength = 100;
@@ -18,13 +22,37 @@ function excerpt(description) {
     : description;
 }
 
-const Comics = ({ onClick }) => {
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "100%",
+  maxWidth: "500px",
+  boxShadow: 24,
+  overflowY: "auto",
+  maxHeight: "100vh",
+};
+
+export default function Comics({ onClick }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleMinting = () => {
+    // TODO
+  };
+
   return (
     <Box>
       <Grid container spacing={3} justifyContent="center" alignItems="stretch">
         {comics.map((c, indx) => (
           <Grid item key={indx} xs={3} md={4}>
-            <Card>
+            <Card onClick={handleOpen}>
               <CardActionArea>
                 <CardMedia
                   component="img"
@@ -47,11 +75,51 @@ const Comics = ({ onClick }) => {
                 </CardContent>
               </CardActionArea>
             </Card>
+
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="child-modal-title"
+              aria-describedby="child-modal-description"
+            >
+              <Box sx={style}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    height="400"
+                    src={`/imgs/${c.img}`}
+                    alt={c.title}
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      className={font.className}
+                    >
+                      {c.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {c.description}
+                    </Typography>
+                  </CardContent>
+
+                  <CardActions>
+                    <Button
+                      style={{ marginInlineStart: "auto" }}
+                      onClick={handleMinting}
+                      variant="contained"
+                      size="large"
+                    >
+                      Mint NFT
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Box>
+            </Modal>
           </Grid>
         ))}
       </Grid>
     </Box>
   );
-};
-
-export default Comics;
+}
