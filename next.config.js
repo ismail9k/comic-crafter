1a0f48bce1ab060f3a01188e9bdee2338c5dad13
ismail9k <dev@ismail9k.com>
2023-08-12 15:35:14 +0300

    chore: update next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    config.module.rules.push({ test: /\.node/, use: "raw-loader" });
     config.resolve.fallback = { ...config.resolve.fallback, canvas: false };
     return config;
   },
